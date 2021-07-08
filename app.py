304c66f4ae2bfcb77aef6d7e14061ef26edf9889
zhangge <zhangge@yingzi.com>
2021-07-08 15:17:12 +0800

    实时接口控制中心

--- app.py
+++ app.py
@@ -1,13 +1,29 @@
 # coding:utf-8
 from flask import Flask, request,render_template
 from controller.conn_flask import blueprint
+from healthcheck import HealthCheck, EnvironmentDump
 
-def create_app():
-    app = Flask(__name__)
-    app.register_blueprint(blueprint, url_prefix='/pigfarm_init')
-    return app
+app = Flask(__name__)
 
+# wrap the flask app and give a heathcheck url
+health = HealthCheck(app, "/healthcheck")
+envdump = EnvironmentDump(app, "/environment")
 
-if __name__ == '__main__':
-    app = create_app()
-    app.run()
+# add your own check function to the healthcheck
+def redis_available():
+    #client = _redis_client()
+    #info = client.info()
+    return True, "redis ok"
+
+health.add_check(redis_available)
+'''
+# add your own data to the environment dump
+def application_data():
+	return {"maintainer": "Frank Stratton",
+	        "git_repo": "https://github.com/Runscope/healthcheck"}
+'''
+envdump.add_section("application", redis_available)
+
+if __name__ == "__main__":
+    app.run(debug=True, host='127.0.0.1', port=8000)
+
